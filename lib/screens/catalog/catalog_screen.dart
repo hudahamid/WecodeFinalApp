b03daec5f9843bd0cdf9076d77a42ad15203949c
hudahamid <huda.00091581@gmail.com>
2022-03-05 01:07:25 +0300

    category

--- lib/screens/catalog/catalog_screen.dart
+++ lib/screens/catalog/catalog_screen.dart
@@ -1,6 +1,7 @@
 
 import 'dart:ui';
 
+import 'package:finalwecodeproject/models/category_model.dart';
 import 'package:finalwecodeproject/widget/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,18 +9,16 @@ import 'package:flutter/material.dart';
 class catalogScreen extends StatelessWidget {
   static  const  String routeName= '/catalog';
   
-  static Route route(){
+  static Route route({required Category category}){
     return  MaterialPageRoute(
       settings: RouteSettings(name: routeName),
-      builder: (_)=> catalogScreen()
+      builder: (_)=> catalogScreen(category:category)
       
       );
-
-
   }
+   final Category category;
 
-
-  const catalogScreen({ Key? key }) : super(key: key);
+  const catalogScreen({ required this.category }) ;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
