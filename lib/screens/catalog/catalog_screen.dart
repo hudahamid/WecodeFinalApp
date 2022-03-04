05fbf27949ac418aa1bea66256d6c0268fd36578
hudahamid <huda.00091581@gmail.com>
2022-03-05 02:00:04 +0300

    categoryy

--- lib/screens/catalog/catalog_screen.dart
+++ lib/screens/catalog/catalog_screen.dart
@@ -2,6 +2,7 @@
 import 'dart:ui';
 
 import 'package:finalwecodeproject/models/category_model.dart';
+import 'package:finalwecodeproject/models/model.dart';
 import 'package:finalwecodeproject/widget/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -18,17 +19,35 @@ class catalogScreen extends StatelessWidget {
   }
    final Category category;
 
-  const catalogScreen({ required this.category }) ;
+  const catalogScreen( { Key? key ,required this.category }) : super(key: key) ; // may have problem here mine
   @override
   Widget build(BuildContext context) {
+    final List<Product> categoryProducts=Product.products.where((product) =>product.category==category.name).toList();
     return Scaffold(
       backgroundColor: Colors.white,
       
-      appBar: customAppbarWiedget(title:'catalog'),
+      appBar: customAppbarWiedget(title:category.name),
      
       bottomNavigationBar: custom_navBar(),
+
+      body:GridView.builder(
+        padding: const EdgeInsets.symmetric(horizontal:8.0 ,vertical:16.0 ),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(  
+                            crossAxisCount: 2,
+                            childAspectRatio: 1.15
+                            ),
+           
+                    itemCount: categoryProducts.length, 
+                    itemBuilder: (BuildContext context,int index){
+                      return productCard(
+                          product: categoryProducts[index],
+                          widthFactor: 2.2,
+                          );
+                    }
+                    
+                    ) 
+      
       
     );
   }
 }
-
